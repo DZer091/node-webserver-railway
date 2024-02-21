ef8a2d633156ca510b01c47de8c3a1971a1dd9ba
DZer091 <lclaverod@outlook.com>
2024-02-20 21:26:51 -0300

    Actualización con comentarios

--- app-old.js
+++ app-old.js
@@ -1,36 +1,36 @@
 const http = require('http');
 
-http.createServer((request, response) =>{//request es la solicitud del cliente, response lo que el seervidor responde al cliente
-   
-
-    //response.setHeader('Content-Disposition','attachment; filename=lista.csv');
-    // response.setHeader('Content-Disposition','attachment; filename=lista.csv');
+http.createServer((request, response) => { //request es la solicitud del cliente, response lo que el seervidor responde al cliente
 
 
-    //response.writeHead(200, {'Content-Type':'application/csv'});
-    // response.writeHead(200, {'Content-Type':'application/csv'});
-    // response.writeHead(200, {'Content-Type':'application/json'});
-
-    // const persona = {
-    //     id:1,
-    //     nombre:'Fernando'
-    // }
-    
-    // response.write( JSON.stringify(persona));
-
-    // response.write('id, nombre\n');
-    // response.write('1, Maneper\n');
-    // response.write('2, Maigo\n');
-    // response.write('3, Luchoto\n');
-    // response.write('4, Mana\n');
-    // response.write('5, Xure\n');
-
-    response.write('WENA');
+        //response.setHeader('Content-Disposition','attachment; filename=lista.csv');
+        // response.setHeader('Content-Disposition','attachment; filename=lista.csv');
 
 
-    response.end();
-})
-.listen(8070);
+        //response.writeHead(200, {'Content-Type':'application/csv'});
+        // response.writeHead(200, {'Content-Type':'application/csv'});
+        // response.writeHead(200, {'Content-Type':'application/json'});
+
+        // const persona = {
+        //     id:1,
+        //     nombre:'Fernando'
+        // }
+
+        // response.write( JSON.stringify(persona));
+
+        // response.write('id, nombre\n');
+        // response.write('1, Maneper\n');
+        // response.write('2, Maigo\n');
+        // response.write('3, Luchoto\n');
+        // response.write('4, Mana\n');
+        // response.write('5, Xure\n');
+
+        response.write('WENA');
 
 
-console.log('Escuchabndo erl puerto 8070');
+        response.end(); //termina de reponder al cliente
+    })
+    .listen(8070);
+
+
+console.log('Escuchabndo el puerto 8070');
--- app.js
+++ app.js
@@ -9,25 +9,25 @@ const port = process.env.PORT;
 
 
 //todo required('hbs') Handlebars
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); //renderiza las vistas a utilizar. En este caso se usa hbs, solo se debe sacar para consumir los archivos .html de la carpeta old-back (backup)
 hbs.registerPartials(__dirname + '/views/partials', function(err) {});
 
 
 // Servir contenido estatico
-app.use(express.static('public')); //va directo a un archivo que se llame index.html
+app.use(express.static('public')); //va directo a un archivo que se llame index.html. En caso de tener la carpeta "public" en otro directorio. Este debe ser rspecificado
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.render('home', {
         nombre: "Zerinho Best",
         titulo: 'Soy el mejor'
     });
 });
 
-app.get('/hola-mundo', function(req, res) {
+app.get('/hola-mundo', (req, res) => {
     res.send('WENA MALO PAL CATRE')
 });
 
-app.get('/generic', function(req, res) {
+app.get('/generic', (req, res) => {
     // res.send('ERRRORRRRR MORIREMOS TODOS')
     // res.sendFile(__dirname + '/public/generic.html')
     res.render('generic', {
@@ -36,7 +36,7 @@ app.get('/generic', function(req, res) {
     });
 });
 
-app.get('/elements', function(req, res) {
+app.get('/elements', (req, res) => {
     // res.send('ERRRORRRRR MORIREMOS TODOS')
     // res.sendFile(__dirname + '/public/elements.html')
     res.render('elements', {
@@ -45,11 +45,15 @@ app.get('/elements', function(req, res) {
     });
 });
 
-app.get('*', function(req, res) {
+app.get('*', (req, res) => { // * = Comodin. En caso que no se encuentre la ruta a acceder
     // res.send('ERRRORRRRR MORIREMOS TODOS')
-    res.sendFile(__dirname + '/public/404.html')
+    res.sendFile(__dirname + '/public/old-back/404.html') //__dirname = es el path donde esta alojada la app (raiz)
 });
 
+// app.get('*', function(req, res) {puedes cambiar la palabra funcion por una funcion de flecha
+//     // res.send('ERRRORRRRR MORIREMOS TODOS')
+//     res.sendFile(__dirname + '/public/404.html')
+// });
 
 
 app.listen(port, () => {
